4cc2c71beb56c2dcbfdf22703fe24e5f3fcb310f
eesfernandes <eesf@hotmail.com>
2024-02-03 12:06:11 -0300

    labeddit-backend

---
+++ src/router/replysRouter.ts
@@ -0,0 +1,24 @@
+import express  from "express";
+import { ReplyBusiness } from '../business/ReplyBusiness'
+import { ReplyController } from '../controller/ReplyController'
+import { ReplyDatabase } from '../database/ReplyDatabase'
+import { IdGenerator } from '../services/IdGenerator'
+import { TokenManager } from '../services/TokenManager'
+
+export const replysRouter = express.Router()
+
+
+const replyController = new ReplyController(
+    new ReplyBusiness(
+        new ReplyDatabase(),
+        new IdGenerator(),
+        new TokenManager()
+    )
+)
+
+
+replysRouter.get("/:id", replyController.getReplysbyId)
+replysRouter.post("/:id", replyController.createReply)
+replysRouter.put("/:id", replyController.editReply)
+replysRouter.delete("/:id", replyController.deleteReply)
+replysRouter.put("/:id/like", replyController.likeOrDislikeReply)
